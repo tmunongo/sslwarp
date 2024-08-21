f1afd40906d8b530669f6521ecece99d76148c48
Tawanda <ejmunongo@gmail.com>
2024-08-21 21:27:15 +0200

    backtrack on Rust migration

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, rust-overlay }:
@@ -17,25 +13,13 @@
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
+          overlays = [ self.overlays.default ];
         };
       });
     in
     {
       overlays.default = final: prev: {
         go = final."go_1_${toString goVersion}";
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [ "rust-src" "rustfmt" ];
-            };
       };
 
       devShells = forEachSupportedSystem ({ pkgs }: {
@@ -51,21 +35,7 @@
             golangci-lint
 
             air
-
-            # rust
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
           ];
-
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-          };
         };
       });
     };
